00c5285e955793c1d20990f2b583e5574eeba4a6
SuneelReddy9999 <162782547+SuneelReddy9999@users.noreply.github.com>
2024-03-09 18:05:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        autoplay: {
+            delay: 5000, // Adjust the delay (in milliseconds) between slides
+            disableOnInteraction: false, // Allow manual navigation when mouse hovered over
+        },
+    });
+</script>
